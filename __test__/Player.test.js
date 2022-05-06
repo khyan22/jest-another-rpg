571472eb715afd7a9097134c8f344a8c35791ef4
khyan22 <khyanalvarez@gmail.com>
2022-05-06 09:06:58 -0400

    added prototype methods

---
+++ __test__/Player.test.js
@@ -0,0 +1,36 @@
+const Player = require('../lib/Player')
+const Potion = require('../lib/Potion')
+
+jest.mock('../lib/Potion')
+
+test('creates a player object', () => {
+    const player = new Player('Khyan')
+
+    expect(player.name).toBe('Khyan')
+    expect(player.health).toEqual(expect.any(Number))
+    expect(player.strength).toEqual(expect.any(Number))
+    expect(player.agility).toEqual(expect.any(Number))
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
+})
+
+test('gets players stats as an object', () => {
+    const player = new Player('Mari')
+
+    expect(player.getStats()).toHaveProperty('potions')
+    expect(player.getStats()).toHaveProperty('health')
+    expect(player.getStats()).toHaveProperty('strength')
+    expect(player.getStats()).toHaveProperty('agility')
+})
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Angel')
+
+    expect(player.getInventory()).toEqual(expect.any(Array))
+
+    player.inventory = []
+
+    expect(player.getInventory()).toEqual(false)
+})
